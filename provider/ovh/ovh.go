7cda078011c8bbddecf1b262818ba2c3e5644c93
Carlos Leyva <66577002+carlos-leyva-guerrero@users.noreply.github.com>
2023-06-02 00:24:41 +0200

    fix syntax

--- provider/ovh/ovh.go
+++ provider/ovh/ovh.go
@@ -314,7 +314,7 @@ func newOvhChange(action int, endpoints []*endpoint.Endpoint, zones []string, re
 		// Check if the DNSName is a TXT entry and is formed "a-domainname.com" being domainname in the zone list to fix the
 		// TXT entries that are created for the A records on the apex.
 		if (zone == "" && e.RecordType == "TXT" && strings.Count(e.DNSName, ".") == 1) {
-			zone2, _ = zoneNameIDMapper.FindZone(strings.TrimPrefix(e.DNSName, "a-")) 
+			zone2, _ := zoneNameIDMapper.FindZone(strings.TrimPrefix(e.DNSName, "a-")) 
 			zone = zone2
 		}
 		if zone == "" {
